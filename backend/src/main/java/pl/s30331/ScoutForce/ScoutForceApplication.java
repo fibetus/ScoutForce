3703de73ea769f6c08ac24d3ee93a33a6e1cc5f6
fibetus <s30331@pjwstk.edu.pl>
2026-05-30 04:09:17 +0200

    add: spring boot project setup with maven and h2 configuration

---
+++ backend/src/main/java/pl/s30331/ScoutForce/ScoutForceApplication.java
@@ -0,0 +1,13 @@
+package pl.s30331.ScoutForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutForceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/s30331/ScoutForce/ScoutForceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.s30331.ScoutForce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
